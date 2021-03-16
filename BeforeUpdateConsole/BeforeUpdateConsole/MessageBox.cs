24515bd8cab24f87636f13a65a985a6a64b70455
joysola <joysola315718801@gmail.com>
2021-03-16 13:24:21 +0800

    1. 上传初版程序，用于安装更新包时进行必要的提示。

---
+++ BeforeUpdateConsole/BeforeUpdateConsole/MessageBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeforeUpdateConsole
+{
+    public class MessageBox
+    {
+        /// <summary>
+        /// 原生方法
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="msg">显示提示</param>
+        /// <param name="caption">标题</param>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        [DllImport("User32.dll", EntryPoint = "MessageBox", CharSet = CharSet.Unicode)]
+        private static extern int MsgBox(IntPtr h, string msg, string caption, uint type);
+
+        public static MessageBoxResult Show(string text)
+        {
+            return (MessageBoxResult)MsgBox(IntPtr.Zero, text, "\0", (uint)MessageBoxButtons.Ok);
+        }
+
+        public static MessageBoxResult Show(string text, string caption)
+        {
+            return (MessageBoxResult)MsgBox(IntPtr.Zero, text, caption, (uint)MessageBoxButtons.Ok);
+        }
+
+        public static MessageBoxResult Show(string text, string caption, MessageBoxButtons buttons = MessageBoxButtons.Ok)
+        {
+            return (MessageBoxResult)MsgBox(IntPtr.Zero, text, caption, (uint)buttons);
+        }
+
+        public static MessageBoxResult Show(string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+           return (MessageBoxResult)MsgBox(IntPtr.Zero, text, caption, ((uint)buttons) | ((uint)icon));
+        }
+
+        public static MessageBoxResult Show(string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon, MessageBoxDefaultButton button)
+        {
+            return (MessageBoxResult)MsgBox(IntPtr.Zero, text, caption, ((uint)buttons) | ((uint)icon) | ((uint)button));
+        }
+
+        public static MessageBoxResult Show(string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon, MessageBoxDefaultButton button, MessageBoxModal modal)
+        {
+            return (MessageBoxResult)MsgBox(IntPtr.Zero, text, caption, ((uint)buttons) | ((uint)icon) | ((uint)button) | ((uint)modal));
+        }
+    }
+}
---
+++ BeforeUpdateConsole/BeforeUpdateConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeforeUpdateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. 获取配置的进程名
+            var programName = ConfigurationManager.AppSettings["ProgramName"]; // 进程名
+            var programChineseName = ConfigurationManager.AppSettings["ProgramChineseName"] ?? programName; // 进程中文名
+            
+            if (string.IsNullOrEmpty(programName)) // 没有配置进程名字，退出
+            {
+                return;
+            }
+
+            // 2. 检测系统进程中 程序是正在运行
+            var processes = Process.GetProcessesByName(programName);
+            if (processes.Length != 0) // 程序已经在运行进行提示，并退出
+            {
+                var dialogResult = MessageBox.Show($"“{programChineseName}”程序正在运行，无法进行更新，请手动关闭“{programChineseName}”程序！",
+                    "警告",
+                    MessageBoxButtons.Ok,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxModal.Application);
+                return;
+            }
+
+            // 3. 启动安装程序
+            Process.Start("setup.exe");
+        }
+    }
+}
